d1fd51e76f5c6dd7cf39a34074fa4162aeb7cda2
Irsyad Zainul Abidin <irsyadza04@gmail.com>
2026-01-13 03:39:30 +0700

    fix: config deploy env

--- bot/src/config.js
+++ bot/src/config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-require('dotenv').config({ path: path.join(__dirname, '../.env') });
+require('dotenv').config({ path: path.join(__dirname, '../../.env') });
 
 const env = process.env.APP_ENV || 'production';
 
